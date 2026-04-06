40f0aa05e4f0232d4e9086386abf259655301c97
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2026-04-06 11:02:29 +0300

    fix: убрать кнопку редактирования со страницы просмотра

--- app/pages/company/index.vue
+++ app/pages/company/index.vue
@@ -15,11 +15,6 @@ onMounted(async () => {
 <template>
   <div class="space-y-6 max-w-2xl">
     <BaseCard>
-      <div class="flex justify-end mb-4">
-        <NuxtLink to="/company/edit" class="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-lg transition-colors">
-          Редактировать
-        </NuxtLink>
-      </div>
       <FormsCompanyForm 
         :company="company" 
         :disabled="true"
